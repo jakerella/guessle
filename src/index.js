bfe608fc31d08f3bccb6be39b0b003c019b68d69
Jordan Kasper <jordan@jordankasper.com>
2022-01-18 18:24:54 -0500

    improved error page and handling; added better comments to server index page

--- src/index.js
+++ src/index.js
@@ -1,55 +1,56 @@
+// Pull in a .env file if present
 require('dotenv').config()
 
 const express = require('express')
-const redis = require('redis')
+const { createClient } = require('redis')
 const session = require('express-session')
 const AppError = require('./util/AppError')
 
-// All the routes
+// Require the routes
 const game = require('./routes/game')
 const stats = require('./routes/stats')
 const history = require('./routes/history')
 
-// env vars and other config
+// set default env vars
 const PORT = process.env['PORT'] || 80
+const SESS_SECRET = process.env.GUESSLE_SESS_SECRET || 'mytimestampbringsalltheboystotheyard-' + Date.now()
 
 
-// start it up
+// Express app setup
 const app = express()
-
 app.use(express.static('static'))
 app.set('view engine', 'pug')
 app.use(express.json())
 
 
+// Set up the express session management with Redis
 let RedisStore = require('connect-redis')(session)
-let redisSessionClient = redis.createClient(process.env.REDIS_URL)
+let redisSessionClient = createClient(process.env.REDIS_URL)
 redisSessionClient.on('connect', () => { console.log('Connected to Redis for session storage') })
 redisSessionClient.on('error', (err) => {
     console.error('Unable to maintain redis connection for session storage. Stopping server.')
     console.error(err.message)
     process.exit(1)
 })
-const sessionOptions = {
-    secret: process.env.GUESSLE_SESS_SECRET,
-    store: (RedisStore) ? new RedisStore({ client: redisSessionClient }) : null,
+app.use(session({
+    secret: SESS_SECRET,
+    store: new RedisStore({ client: redisSessionClient }),
     resave: false,
     name: 'guessle',
     saveUninitialized: false
-}
-app.use(session(sessionOptions))
+}))
 
 
+// Add in our routes
 app.use('/stats', stats)
 app.use('/history', history)
-app.use('/', game)
+app.use('/', game)  // the game is mounted to the root route, so needs to be last
 
 
-// 404 catcher
+// 404 catcher, then error catchall
 app.use((req, res, next) => {
     next(new AppError('Sorry, but I could not find that page.', 404))
 })
-
 app.use((err, req, res, next) => {
     if (!err.status || err.status > 499) {
         console.error(err)
@@ -59,7 +60,7 @@ app.use((err, req, res, next) => {
     res.render('error', {
         page: 'error',
         title: 'Error',
-        message: (err.status === 500) ? 'Sorry, we ran into a problem.' : err.message
+        errorMessage: (err.status === 500) ? 'Sorry, we ran into a problem.' : err.message
     })
 })
 
