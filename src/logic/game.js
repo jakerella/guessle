9d2c8f0cbb9c41e52dfb7ee0c01ae53685c0ac53
Jordan Kasper <jordan@jordankasper.com>
2022-01-18 14:16:06 -0500

    fix bug where two letters are marked yellow when only 1 exists in the answer

--- src/logic/game.js
+++ src/logic/game.js
@@ -8,7 +8,7 @@ const frequent = {
     6: require('../../lists/frequent_6.json')
 }
 
-const check = {
+const CHECK_TYPES = {
     'NOPE': 0,
     'SORTA': 1,
     'YAS': 2
@@ -41,43 +41,47 @@ module.exports = {
         }
     },
 
-    makeGuess(game, word) {
-        if (!dict[game.word.length].includes(word)) {
+    makeGuess(game, guessWord) {
+        if (!dict[game.word.length].includes(guessWord)) {
             throw new AppError('Sorry, but I don\'t know that word.', 400)
         }
-        const prevGuess = game.guesses.map((guess) => guess.map((g) => g.letter).join('')).filter((g) => g === word)
-        if (prevGuess.length) {
+        const prevGuessMatch = game.guesses.map((gameGuess) => gameGuess.map((g) => g.letter).join('')).filter((g) => g === guessWord)
+        if (prevGuessMatch.length) {
             throw new AppError('You already guessed that word!', 400)
         }
 
         const guess = []
-        const stats = {}
         for (let i=0; i<game.word.length; ++i) {
-            if (!stats[game.word[i]]) {
-                stats[game.word[i]] = { count: 0, y: 0, n: 0, s: 0 }
-            }
-            stats[game.word[i]].count++
-            
-            if (word[i] === game.word[i]) {
-                guess.push({ letter: word[i], check: check.YAS })
-                stats[game.word[i]].y++
-            } else if (game.word.includes(word[i])) {
-                guess.push({ letter: word[i], check: check.SORTA })
-                stats[game.word[i]].s++
+            if (guessWord[i] === game.word[i]) {
+                guess.push({ letter: guessWord[i], check: CHECK_TYPES.YAS })
+            } else if (game.word.includes(guessWord[i])) {
+                guess.push({ letter: guessWord[i], check: CHECK_TYPES.SORTA })
             } else {
-                guess.push({ letter: word[i], check: check.NOPE })
-                stats[game.word[i]].n++
+                guess.push({ letter: guessWord[i], check: CHECK_TYPES.NOPE })
             }
         }
 
         // Check the sorta's, might have some dupes in here with correct guesses (so they're false-sortas)
-        const sortas = guess.filter((gl) => gl.check === check.SORTA)
-        sortas.forEach((sorta) => {
-            if (stats[sorta.letter].count === stats[sorta.letter].y) {
-                // this is no sorta... user already found all these letters
-                sorta.check = check.NOPE
-            }
-        })
+        // Start with the green ("yas") letters for the counts
+        const letterCounts = {}
+        guess
+            .filter((gl) => gl.check === CHECK_TYPES.YAS)
+            .forEach((gl) => {
+                letterCounts[gl.letter] = (letterCounts[gl.letter]) ? ++letterCounts[gl.letter] : 1
+            })
+        guess
+            // grab all the "sortas"
+            .filter((gl) => gl.check === CHECK_TYPES.SORTA)
+            .forEach((sorta) => {
+                const answerLetterCount = game.word.match(new RegExp(sorta.letter, 'g')).length
+                // Add the sortas to the "correct" answer count...
+                letterCounts[sorta.letter] = (letterCounts[sorta.letter]) ? ++letterCounts[sorta.letter] : 1
+                // And determine if we have too many "sortas" for the given letter
+                if (answerLetterCount < letterCounts[sorta.letter]) {
+                    sorta.check = CHECK_TYPES.NOPE
+                    letterCounts[sorta.letter]--
+                }
+            })
 
         return guess
     },
